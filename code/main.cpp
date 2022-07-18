295083335e6b50d6f0db272741038016f67e9113
mtien314 <90877954+mtien314@users.noreply.github.com>
2022-07-18 18:01:56 +0700

    Add files via upload

---
+++ code/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include"Stack.h"
+#include"Infix.h"
+using namespace std;
+
+
+int main()
+{
+	string x;
+	cout << "Input: ";
+	getline(cin, x);
+	vector<string> A;
+	Infix p;
+	p.SplitSTring(x, A);
+	p.Infix1(A);
+	return 0;
+}
